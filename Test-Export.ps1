53b0b0367a2fe73f032faef85f3f5f3f999567bf
RuiRomano <RuiRomano@microsoft.com>
2023-10-30 19:42:33 +0000

    export for BPA rules

--- Test-Export.ps1
+++ Test-Export.ps1
@@ -6,8 +6,6 @@ Import-Module ".\FabricPS-PBIP" -Force
 
 # Set-FabricAuthToken -reset
 
-# https://msit.powerbi.com/groups/cdfc383c-5eaa-4f39-91de-0eb26fdd2401
-
 $workspaceId = "c45c04b0-4fe8-4566-bc78-0f768872aeaf"
 
 Export-FabricItems -workspaceId $workspaceId -path '.\export'
--- Test-Import.ps1
+++ Test-Import.ps1
@@ -4,8 +4,6 @@ Set-Location $currentPath
 
 Import-Module ".\FabricPS-PBIP" -Force
 
-# https://msit.powerbi.com/groups/cdfc383c-5eaa-4f39-91de-0eb26fdd2401
-
 $workspaceId = "cdfc383c-5eaa-4f39-91de-0eb26fdd2401"
 
 Import-FabricItems -workspaceId $workspaceId -path '.\SamplePBIP' -filter "*Sales.Report*"
